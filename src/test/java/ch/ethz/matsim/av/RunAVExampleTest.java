d8e72596b5c21da07b5c7fa0d92552c6f21324c0
Sebastian Hörl <sebastian.hoerl@ivt.baug.ethz.ch>
2017-08-03 10:15:02 +0200

    Travis unit testing (#9)
    Added faster unit tests, remove Sioux Falls dependency and made it work with Travis

--- src/test/java/ch/ethz/matsim/av/RunAVExampleTest.java
+++ src/test/java/ch/ethz/matsim/av/RunAVExampleTest.java
@@ -3,7 +3,9 @@ package ch.ethz.matsim.av;
 import ch.ethz.matsim.av.framework.AVConfigGroup;
 import ch.ethz.matsim.av.framework.AVModule;
 import ch.ethz.matsim.av.framework.AVQSimProvider;
-import ch.ethz.matsim.sioux_falls.SiouxFallsUtils;
+import ch.ethz.matsim.av.scenario.TestScenarioAnalyzer;
+import ch.ethz.matsim.av.scenario.TestScenarioGenerator;
+import org.junit.Assert;
 import org.junit.Test;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
@@ -13,7 +15,6 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.scenario.ScenarioUtils;
 
 public class RunAVExampleTest {
     @Test
@@ -21,23 +22,24 @@ public class RunAVExampleTest {
         AVConfigGroup avConfigGroup = new AVConfigGroup();
         avConfigGroup.setConfigURL(getClass().getResource("/ch/ethz/matsim/av/av.xml"));
 
-        Config config = ConfigUtils.loadConfig(SiouxFallsUtils.getConfigURL(), avConfigGroup, new DvrpConfigGroup());
-        config.controler().setLastIteration(1);
+        Config config = ConfigUtils.createConfig(avConfigGroup, new DvrpConfigGroup());
+        Scenario scenario = TestScenarioGenerator.generateWithAVLegs(config);
 
         PlanCalcScoreConfigGroup.ModeParams modeParams = config.planCalcScore().getOrCreateModeParams(AVModule.AV_MODE);
         modeParams.setMonetaryDistanceRate(0.0);
         modeParams.setMarginalUtilityOfTraveling(8.86);
         modeParams.setConstant(0.0);
 
-        config.subtourModeChoice().setModes(new String[] { "av" } );
-
-        Scenario scenario = ScenarioUtils.loadScenario(config);
-
         Controler controler = new Controler(scenario);
         controler.addOverridingModule(VrpTravelTimeModules.createTravelTimeEstimatorModule());
         controler.addOverridingModule(new DynQSimModule<>(AVQSimProvider.class));
         controler.addOverridingModule(new AVModule());
 
+        TestScenarioAnalyzer analyzer = new TestScenarioAnalyzer();
+        controler.addOverridingModule(analyzer);
+
         controler.run();
+
+        Assert.assertEquals(0, analyzer.numberOfDepartures - analyzer.numberOfArrivals);
     }
 }
---
+++ src/test/java/ch/ethz/matsim/av/scenario/TestScenarioAnalyzer.java
@@ -0,0 +1,33 @@
+package ch.ethz.matsim.av.scenario;
+
+import org.matsim.api.core.v01.events.PersonArrivalEvent;
+import org.matsim.api.core.v01.events.PersonDepartureEvent;
+import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
+import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.MatsimEventsReader;
+
+public class TestScenarioAnalyzer extends AbstractModule implements PersonDepartureEventHandler, PersonArrivalEventHandler {
+    public long numberOfDepartures;
+    public long numberOfArrivals;
+
+    @Override
+    public void handleEvent(PersonArrivalEvent event) {
+        numberOfArrivals++;
+    }
+
+    @Override
+    public void handleEvent(PersonDepartureEvent event) {
+        numberOfDepartures++;
+    }
+
+    @Override
+    public void reset(int iteration) {}
+
+    @Override
+    public void install() {
+        addEventHandlerBinding().toInstance(this);
+    }
+}
