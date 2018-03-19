4eb2b2cfa48bbcc4141420442c3323de53867abf
Sebastian Hörl <sebastian.hoerl@ivt.baug.ethz.ch>
2018-03-19 16:36:00 +0100

    Make AVTransit events useable by EventsReader (#46)
    * Make events useful for EventsReader
    
    * Make AVTransit events useable by EventsReader

--- src/main/java/ch/ethz/matsim/av/schedule/AVTransitEvent.java
+++ src/main/java/ch/ethz/matsim/av/schedule/AVTransitEvent.java
@@ -1,41 +1,68 @@
 package ch.ethz.matsim.av.schedule;
 
-import ch.ethz.matsim.av.passenger.AVRequest;
+import java.util.Map;
+
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.events.Event;
+import org.matsim.api.core.v01.events.GenericEvent;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.api.internal.HasPersonId;
 
-import java.util.Map;
+import ch.ethz.matsim.av.data.AVOperator;
+import ch.ethz.matsim.av.passenger.AVRequest;
 
-public class AVTransitEvent extends Event implements HasPersonId {
-    final private AVRequest request;
+public class AVTransitEvent extends GenericEvent implements HasPersonId {
+	final private AVRequest request;
 
-    public AVTransitEvent(AVRequest request, double time) {
-        super(time);
-        this.request = request;
-    }
+	final private Id<Person> personId;
+	final private Id<AVOperator> operatorId;
+	final private double distance;
 
-    public AVRequest getRequest() {
-        return request;
-    }
+	public AVTransitEvent(AVRequest request, double time) {
+		this(request.getPassenger().getId(), request.getOperator().getId(), request.getRoute().getDistance(), time,
+				request);
+	}
 
-    @Override
-    public String getEventType() {
-        return "AVTransit";
-    }
+	public AVTransitEvent(Id<Person> personId, Id<AVOperator> operatorId, double distance, double time) {
+		this(personId, operatorId, distance, time, null);
+	}
 
-    @Override
-    public Map<String, String> getAttributes() {
-        Map<String, String> attr = super.getAttributes();
-        attr.put("person", request.getPassenger().getId().toString());
-        attr.put("operator", request.getOperator().getId().toString());
-        attr.put("distance", String.valueOf(request.getRoute().getDistance()));
-        return attr;
-    }
+	private AVTransitEvent(Id<Person> personId, Id<AVOperator> operatorId, double distance, double time,
+			AVRequest request) {
+		super("AVTransit", time);
 
-    @Override
-    public Id<Person> getPersonId() {
-        return request.getPassenger().getId();
-    }
+		this.request = request;
+		this.personId = personId;
+		this.operatorId = operatorId;
+		this.distance = distance;
+	}
+
+	public AVRequest getRequest() {
+		if (request == null) {
+			throw new IllegalStateException();
+		}
+
+		return request;
+	}
+
+	@Override
+	public Map<String, String> getAttributes() {
+		Map<String, String> attr = super.getAttributes();
+		attr.put("person", personId.toString());
+		attr.put("operator", operatorId.toString());
+		attr.put("distance", String.valueOf(distance));
+		return attr;
+	}
+
+	@Override
+	public Id<Person> getPersonId() {
+		return personId;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public Id<AVOperator> getOperatorId() {
+		return operatorId;
+	}
 }
