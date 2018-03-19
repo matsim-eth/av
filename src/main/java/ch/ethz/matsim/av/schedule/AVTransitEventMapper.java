ced17d997ec4917d5ea46bb5cf6e36d330850013
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-03-19 16:30:01 +0100

    Make events useful for EventsReader

---
+++ src/main/java/ch/ethz/matsim/av/schedule/AVTransitEventMapper.java
@@ -0,0 +1,18 @@
+package ch.ethz.matsim.av.schedule;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.GenericEvent;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.events.EventsReaderXMLv1.CustomEventMapper;
+
+import ch.ethz.matsim.av.data.AVOperator;
+
+public class AVTransitEventMapper implements CustomEventMapper<AVTransitEvent> {
+	@Override
+	public AVTransitEvent apply(GenericEvent event) {
+		Id<Person> personId = Id.create(event.getAttributes().get("person"), Person.class);
+		Id<AVOperator> operatorId = Id.create(event.getAttributes().get("operator"), AVOperator.class);
+		double distance = Double.parseDouble(event.getAttributes().get("distance"));
+		return new AVTransitEvent(personId, operatorId, distance, event.getTime());
+	}
+}
